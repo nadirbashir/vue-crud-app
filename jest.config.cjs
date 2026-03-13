125118352463ef7fbee91a25251584099d4637b6
nadirbashir <nadirbashir.pro@gmail.com>
2026-03-13 16:26:49 +0500

    configure jest with Vue3

---
+++ jest.config.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.vue$': '@vue/vue3-jest',
+    '^.+\\js$': 'babel-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
+  moduleFileExtensions: ['vue', 'js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverage: true,
+
+  collectCoverageFrom: ['src/**/*.{js,vue}', '!src/main.js'],
+
+  coverageDirectory: 'coverage',
+
+  coverageReporters: ['text', 'lcov', 'html'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/tests/'],
+  // coverageReporters: ['text', 'json-summary'],
+  // Fix in order for vue-test-utils to work with Jest 29
+  // https://test-utils.vuejs.org/migration/#test-runners-upgrade-notes
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'node-addons'],
+  },
+}
